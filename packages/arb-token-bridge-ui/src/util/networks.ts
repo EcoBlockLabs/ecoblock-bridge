825c96748379fcdc45680366e331e7f49a9f1040
Brian <briandmauro@gmail.com>
2023-05-18 22:11:58 +0700

    Add .env file

--- packages/arb-token-bridge-ui/src/util/networks.ts
+++ packages/arb-token-bridge-ui/src/util/networks.ts
@@ -12,10 +12,6 @@ import { isUserRejectedError } from './isUserRejectedError'
 
 export const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_KEY
 
-if (typeof INFURA_KEY === 'undefined') {
-  throw new Error('Infura API key not provided')
-}
-
 const MAINNET_INFURA_RPC_URL = `https://mainnet.infura.io/v3/${INFURA_KEY}`
 const GOERLI_INFURA_RPC_URL = `https://goerli.infura.io/v3/${INFURA_KEY}`
 
