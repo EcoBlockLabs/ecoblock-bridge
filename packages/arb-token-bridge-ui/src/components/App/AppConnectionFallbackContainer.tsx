0987aeb5418c89fb822234c34f05e61885d989a5
Brian <briandmauro@gmail.com>
2023-05-29 07:58:17 +0700

    Use mixBlendMode for intro image

--- packages/arb-token-bridge-ui/src/components/App/AppConnectionFallbackContainer.tsx
+++ packages/arb-token-bridge-ui/src/components/App/AppConnectionFallbackContainer.tsx
@@ -8,7 +8,8 @@ export function AppConnectionFallbackContainer({
     // className: 'sm:w-[420px]',
     src: EcoBlockIntroImg,
     alt: 'EcoBlock Intro Image',
-    priority: true
+    priority: true,
+    style: { mixBlendMode: 'screen' }
   },
   children
 }: {
